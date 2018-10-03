b907b9951f5bd83aae218f6afb6fcfa9def207e3
Ryan Williams <rpwilliams96@gmail.com>
2018-10-02 20:05:05 -0700

    Set up test folder

---
+++ app/test/test.js
@@ -0,0 +1,8 @@
+var assert = require('assert');
+describe('sum', function() {
+	describe('#2plus2', function() {
+		it('2 plus 2 should equal four', function() {
+			assert.equal(4, 2 + 3);
+		});
+	});
+});
