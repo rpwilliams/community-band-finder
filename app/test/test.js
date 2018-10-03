8d47e2376f9fd726b1462c743a790b4402121655
Ryan Williams <rpwilliams96@gmail.com>
2018-10-02 20:19:15 -0700

    Travis test 5

--- app/test/test.js
+++ app/test/test.js
@@ -2,7 +2,7 @@ var assert = require('assert');
 describe('sum', function() {
 	describe('#2plus2', function() {
 		it('2 plus 2 should equal four', function() {
-			assert.equal(4, 2 + 4);
+			assert.equal(4, 4);
 		});
 	});
 });
@@ -10,7 +10,7 @@ describe('sum', function() {
 describe('REST', function() {
 	describe('#GET', function() {
 		it('2 plus 2 should equal four', function() {
-			assert.equal(4, 2 + 4);
+			assert.equal(4, 4);
 		});
 	});
 });
