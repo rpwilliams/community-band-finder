f3d6042b248951e71735eb5025d6096bd62849cb
rpwilliams <rpwilliams@gmail.com>
2018-12-27 15:33:40 -0800

    CI Fix 2

--- test/test.js
+++ test/test.js
@@ -1,13 +1,13 @@
-// var assert = require('assert');
+var assert = require('assert');
 
-// describe('API', function() {
-// 	describe('#GET', function() {
-// 		it('GET /search/<zipcode> does not return all bands with that zipcode', function() {
-// 			assert.equal(4, 4); // temporary placeholder
-// 		});
+describe('API', function() {
+	describe('#GET', function() {
+		it('GET /search/<zipcode> does not return all bands with that zipcode', function() {
+			assert.equal(4, 4); // temporary placeholder
+		});
 
-// 		it('GET /search/<zipcode> returns a zipcode that does not match the given zipcode', function() {
-// 			assert.equal(4, 4); // temporary placeholder
-// 		});
-// 	});
-// });
+		it('GET /search/<zipcode> returns a zipcode that does not match the given zipcode', function() {
+			assert.equal(4, 4); // temporary placeholder
+		});
+	});
+});
